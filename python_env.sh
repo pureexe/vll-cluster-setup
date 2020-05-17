63aa81721e338dffa1812730f2d0dadfa573d7bf
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-17 22:57:25 +0700

    Fix pytorch

--- python_env.sh
+++ python_env.sh
@@ -1,20 +1,7 @@
-sudo apt install virtualenv
-"""
-# install tensorflow
-tf_version = (1.14 1.15 2.0 2.1)
-for version in tf_version
-do
-    virtualenv /home/vll/venv_tf${version}
-    source /home/vll/venv_tf1.14/bin/activate
-    pip install tensorflow-gpu==${version}
-    pip install -r ${PWD}/vision_package.txt
-    deactivate
-done
-"""
-
-virtualenv /home/vll/venv_torch
+sudo apt install python3 python3-pip virtualenv
+virtualenv -p python3 /home/vll/venv_torch
 source /home/vll/venv_torch/bin/activate
-pip install pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r ${PWD}/vision_package.txt
 pip install git+https://github.com/pureexe/cluster-dataset
 deactivate
