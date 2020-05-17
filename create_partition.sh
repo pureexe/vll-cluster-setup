f2d5704918ef857864e6487ced57f9588af76544
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-17 21:57:45 +0700

    NO SPACE IN VARIBLE

--- create_partition.sh
+++ create_partition.sh
@@ -1,7 +1,7 @@
 #this thing work only  fresh HDD, unformated
 # https://www.digitalocean.com/community/tutorials/how-to-partition-and-format-storage-devices-in-linux
 #6TB HDD
-hdd = /dev/sda 
+hdd=/dev/sda 
 sudo parted ${hdd} mklabel gpt
 sudo parted ${hdd} mkpart primary 1MiB 1000MiB
 sudo parted ${hdd} mkpart primary ext4 1000MiB 100%
--- user_add.sh
+++ user_add.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # password is generate from  echo "<DEFAULT PASSWORD HERE>" | openssl passwd -1 -stdin
-password = $1$XL98CFTz$nTH0WjjTiUl4nMrA8nksf1
-users = (support godsom lunark pakkapon need_yakiniku nontawat heisenberg supasorn mint penguin w nakorn tan)
+password=$1$XL98CFTz$nTH0WjjTiUl4nMrA8nksf1
+users=(support godsom lunark pakkapon need_yakiniku nontawat heisenberg supasorn mint penguin w nakorn tan)
 
 # add vll for environment setup
 sudo useradd vll -p ${password}
