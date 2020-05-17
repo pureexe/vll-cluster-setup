3134888975a187898bceb1e67e7465f8cd78e242
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-17 22:11:56 +0700

    fresh disk need create ext4

--- create_partition.sh
+++ create_partition.sh
@@ -3,9 +3,13 @@
 #6TB HDD
 hdd=/dev/sda 
 sudo parted ${hdd} mklabel gpt
-sudo parted ${hdd} mkpart primary 1MiB 1000MiB
-sudo parted ${hdd} mkpart primary ext4 1000MiB 100%
+sudo parted ${hdd} mkpart primary ext4 1MiB 1000Gib
+sudo parted ${hdd} mkpart primary ext4 1000Gib 100%
+mkfs.ext4 /dev/sda1
+mkfs.ext4 /dev/sda2
 echo "${hdd}1 /data             ext4    defaults              0       0" | sudo tee -a /etc/fstab
 echo "${hdd}2 /data             ext4    defaults              0       0" | sudo tee -a /etc/fstab
+sudo mkdir -p /home2
+sudo mkdir -p /data
 sudo mount ${hdd}1 /home2
-sudo mount ${hdd}1 /data
+sudo mount ${hdd}2 /data
