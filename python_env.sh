e2d0f05cd4762d413fbdff2d007dc67c3cfc05a5
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-17 22:15:10 +0700

    add: cluster dataset

--- python_env.sh
+++ python_env.sh
@@ -16,4 +16,5 @@ virtualenv /home/vll/venv_torch
 source /home/vll/venv_torch/bin/activate
 pip install pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r ${PWD}/vision_package.txt
+pip install git+https://github.com/pureexe/cluster-dataset
 deactivate
