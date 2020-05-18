2d81c6f212ac41ef57c7a5dd7c05284c6f613cce
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-18 10:43:31 +0700

    Fix: pytorch env

--- python_env.sh
+++ python_env.sh
@@ -1,7 +1,7 @@
-sudo apt install python3 python3-pip virtualenv
-sudo virtualenv -p python3 /home/vll/venv_torch
-sudo chmod -R 777 /home/vll/venv_torch
-source /home/vll/venv_torch/bin/activate
+sudo apt install python3 python3-pip virtualenv apache2
+virtualenv --system-site-packages -p python3 /home/vll/venv_pytorch
+sudo chmod -R 777 /home/vll/venv_pytorch
+source /home/vll/venv_pytorch/bin/activate
 pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r ${PWD}/vision_package.txt
 pip install git+https://github.com/pureexe/cluster-dataset
