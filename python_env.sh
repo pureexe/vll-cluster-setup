215f1de162c91c70d2cedf6c227baa7a67445622
Pakkapon Phongthawee <me@pureapp.in.th>
2020-05-18 10:45:19 +0700

    fix: sudo permission

--- python_env.sh
+++ python_env.sh
@@ -1,5 +1,5 @@
 sudo apt install python3 python3-pip virtualenv apache2
-virtualenv --system-site-packages -p python3 /home/vll/venv_pytorch
+sudo virtualenv --system-site-packages -p python3 /home/vll/venv_pytorch/
 sudo chmod -R 777 /home/vll/venv_pytorch
 source /home/vll/venv_pytorch/bin/activate
 pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
