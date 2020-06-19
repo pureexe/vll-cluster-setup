6430bff6d4cf3b94aeafd22306b9e4cd7f66b7e4
Pakkapon Phongthawee <me@pureapp.in.th>
2020-06-19 11:25:38 +0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,6 +1,10 @@
+# Remove previous exist driver
 sudo apt purge nvidia
 sudo apt autoremove
 sudo apt purge cuda
 sudo apt remove nvidia
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-sudo sh cuda_10.2.89_440.33.01_linux.run
+
+# Download & Install CUDA 
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run 
+sudo sh cuda_10.2.89_440.33.01_linux.run --silent --driver --toolkit
+rm -f cuda_10.2.89_440.33.01_linux.run
