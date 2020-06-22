78f5c6f54d5ccec5299cc3c20b3eb5c38494f6b6
Pakkapon Phongthawee <me@pureapp.in.th>
2020-06-22 20:30:51 +0700

    Update cudnn.sh

--- cudnn.sh
+++ cudnn.sh
@@ -1,6 +1,6 @@
 
 # NVIDIA CDNN
-wget http://10.204.100.111:9999/cudnn-10.2-linux-x64-v7.6.5.32.tgz
+wget http://10.204.100.111/setup/cudnn-10.2-linux-x64-v7.6.5.32.tgz
 tar -xzvf cudnn-10.2-linux-x64-v7.6.5.32.tgz
 cp cuda/include/cudnn.h /usr/local/cuda/include
 cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
