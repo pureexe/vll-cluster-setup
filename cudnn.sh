391c0863d00959cf02a0b003ba3e9eb1689b8e82
Pakkapon Phongthawee <pure.gif@gmail.com>
2021-07-15 16:18:23 +0700

    Update cudnn.sh

--- cudnn.sh
+++ cudnn.sh
@@ -1,7 +1,7 @@
 
 # NVIDIA CDNN
-wget http://10.204.100.111/setup/cudnn-10.2-linux-x64-v7.6.5.32.tgz
-tar -xzvf cudnn-10.2-linux-x64-v7.6.5.32.tgz
+wget http://10.204.100.111/setup/cudnn-11.4-linux-x64-v8.2.2.26.tgz
+tar -xzvf cudnn-11.4-linux-x64-v8.2.2.26.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
