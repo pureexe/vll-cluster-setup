b9c386c4ac05069dbf31b2f6547a8e7f2ead0773
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-02-08 09:38:52 +0700

    Update pytorch1.10.sh

--- pytorch1.10.sh
+++ pytorch1.10.sh
@@ -1,5 +1,5 @@
 sudo apt install virtualenv apache2 tmux curl sshfs sshpass libsm6 libxrender1 libfontconfig1 ffmpeg
-sudo virtualenv --system-site-packages -p python3.9 /home/vll/venv_pytorch1.9/
+sudo virtualenv --system-site-packages -p python3.9 /home/vll/venv_pytorch1.10
 sudo chmod -R 777 /home/vll/venv_pytorch1.9
 source /home/vll/venv_pytorch1.9/bin/activate
 pip install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
