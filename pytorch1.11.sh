940de0f7a37c22e123dd2d0462e8bfd2decc0ae0
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-04-22 14:19:41 +0700

    Update pytorch1.11.sh

--- pytorch1.11.sh
+++ pytorch1.11.sh
@@ -5,7 +5,7 @@ source /home/vll/venv_pytorch1.11/bin/activate
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip install pytorch-lightning
 pip install -r ${PWD}/vision_package.txt
-pip install ruamel.yaml kornia imageio-ffmpeg tensorboardX configargs
+pip install ruamel.yaml kornia imageio-ffmpeg tensorboardX configargparse
 pip install git+https://github.com/pureexe/cluster-dataset
 deactivate
 
