43950da451f0de3b184a6252096578abf578b571
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-04-22 14:16:48 +0700

    Update pytorch1.11.sh

--- pytorch1.11.sh
+++ pytorch1.11.sh
@@ -1,7 +1,7 @@
 sudo apt install virtualenv apache2 tmux curl sshfs sshpass libsm6 libxrender1 libfontconfig1 ffmpeg
 sudo virtualenv --system-site-packages -p python3.9 /home/vll/venv_pytorch1.11
-sudo chmod -R 777 /home/vll/venv_pytorch1.10
-source /home/vll/venv_pytorch1.10/bin/activate
+sudo chmod -R 777 /home/vll/venv_pytorch1.11
+source /home/vll/venv_pytorch1.11/bin/activate
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip install pytorch-lightning
 pip install -r ${PWD}/vision_package.txt
