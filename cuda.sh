e38bc223744336a19f52da12c664e2c93aa53d1e
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-06-18 15:48:05 +0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -5,6 +5,6 @@ sudo apt purge cuda
 sudo apt remove nvidia
 
 # Download & Install CUDA 
-wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda_11.4.0_470.42.01_linux.run
-sudo sh cuda_11.4.0_470.42.01_linux.run --silent --driver --toolkit
-rm -f cuda_11.4.0_470.42.01_linux.run
+wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
+sudo sh cuda_11.3.0_465.19.01_linux.run --silent --driver --toolkit
+rm -f cuda_11.3.0_465.19.01_linux.run
