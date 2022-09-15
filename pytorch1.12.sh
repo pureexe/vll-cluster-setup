20fddd3e3f1ec8658024277f2324c0c9c7be38a6
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-09-15 21:23:46 +0700

    Create pytorch1.12.sh

---
+++ pytorch1.12.sh
@@ -0,0 +1,11 @@
+sudo apt install virtualenv apache2 tmux curl sshfs sshpass libsm6 libxrender1 libfontconfig1 ffmpeg
+sudo virtualenv --system-site-packages -p python3.9 /home/vll/venv_pytorch1.12
+sudo chmod -R 777 /home/vll/venv_pytorch1.12
+source /home/vll/venv_pytorch1.12/bin/activate
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip install pytorch-lightning
+pip install -r ${PWD}/vision_package.txt
+pip install ruamel.yaml kornia imageio-ffmpeg tensorboardX configargparse
+pip install git+https://github.com/pureexe/cluster-dataset
+deactivate
+
