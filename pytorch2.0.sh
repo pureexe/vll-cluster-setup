fd2d53f1da47344c74d6f4198b49dc69f765f4a2
Pakkapon Phongthawee <pure.gif@gmail.com>
2023-04-30 16:20:49 +0700

    Update pytorch2.0.sh

--- pytorch2.0.sh
+++ pytorch2.0.sh
@@ -4,6 +4,7 @@ sudo chmod -R 777 /home/vll/venv_pytorch2.0
 source /home/vll/venv_pytorch2.0/bin/activate
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install pytorch-lightning
+pip install -U diffusers transformers datasets accelerate xformers 
 pip install -r ${PWD}/vision_package.txt
 pip install ruamel.yaml kornia imageio-ffmpeg tensorboardX configargparse
 pip install git+https://github.com/pureexe/cluster-dataset
