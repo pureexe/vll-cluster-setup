2db5ce117853cf7b0c037feabee1537350854863
Pakkapon Phongthawee <pure.gif@gmail.com>
2023-10-31 03:17:24 +0700

    Update setup_pytorch2.0_py3.10.sh

--- setup_pytorch2.0_py3.10.sh
+++ setup_pytorch2.0_py3.10.sh
@@ -1,9 +1,9 @@
 # create environment
-sudo virtualenv --system-site-packages -p python3.10 /home/vll/venv_pytorch2.0_py3.10
-sudo chmod -R 777 /home/vll/venv_pytorch2.0_py3.10
+sudo virtualenv --system-site-packages -p python3.10 /home/vll/venv_pytorch2.0
+sudo chmod -R 777 /home/vll/venv_pytorch2.0
 
 # activate virtualenv
-source /home/vll/venv_pytorch2.0_py3.10/bin/activate
+source /home/vll/venv_pytorch2.0/bin/activate
 
 # install pytorch package
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
@@ -12,7 +12,7 @@ pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https
 pip install pytorch_lightning==2.0.5
 
 # install diffuser-related package
-pip install diffusers==0.18.2
+pip install diffusers==0.21.0
 pip install datasets==2.13.1
 pip install accelerate==0.21.0
 pip install xformers==0.0.20
@@ -30,6 +30,9 @@ pip install IPython==8.14.0
 pip install tensorboard==2.13.0
 pip install tensorboardX==2.6.1
 pip install matplotlib==3.7.2
+
+# install natsort
+pip install natsort
 
 # cleaning up
 deactivate
