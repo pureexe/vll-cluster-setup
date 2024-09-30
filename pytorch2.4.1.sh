3f06524b3cad4de0b17aef0ac322b75e79a1cc1a
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-09-30 10:36:10 +0700

    Update pytorch2.4.1.sh

--- pytorch2.4.1.sh
+++ pytorch2.4.1.sh
@@ -1,4 +1,4 @@
-sudo apt install virtualenv apache2 tmux curl sshfs sshpass libsm6 libxrender1 libfontconfig1 ffmpeg
+sudo apt install virtualenv apache2 tmux curl sshfs sshpass libsm6 libxrender1 libfontconfig1 ffmpeg lib-openexr
 
 sudo virtualenv --system-site-packages -p python3.11 /home/vll/venv_pytorch2.4.1
 
@@ -15,6 +15,9 @@ pip install bitsandbytes==0.44.0
 pip install -U transformers==4.45.1 datasets==3.0.1 accelerate==0.34.2 xformers==0.0.28
 
 pip install -U diffusers==0.30.3
+
+pip install openexr-python
+pip install skylibs
 
 pip install natsort
 pip install -r ${PWD}/vision_package.txt
